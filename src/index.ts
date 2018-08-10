276678b95799ee053736a245fefa3ada4b4848ce
Edward Carroll <ed@edcarroll.co.uk>
2018-08-11 02:43:07 +0800

    feat: Removed all references to `global`, added `TaJson` export.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export * from "./json";
 export * from "./types";
 export * from "./decorators";
 export * from "./converters";
+
+// Export both as JSON name but also local name.
+export { TaJson as JSON, TaJson } from "./ta-json";
--- src/ta-json.ts
+++ src/ta-json.ts
@@ -1,14 +1,14 @@
 import { serialize } from "./methods/serialize";
 import { deserialize } from "./methods/deserialize";
-import { JsonValue, IParseOptions } from "./types";
+import { IParseOptions } from "./types";
 
-export class JSON {
+export class TaJson {
     public static deserialize<T>(object:any, type?:Function, options?:IParseOptions):T {
         return deserialize(object, type, options);
     }
 
     public static parse<T>(json:string, type?:Function, options?:IParseOptions):T {
-        return this.deserialize<T>(global.JSON.parse(json), type, options);
+        return this.deserialize<T>(JSON.parse(json), type, options);
     }
 
     public static serialize(value:any):any {
@@ -16,6 +16,6 @@ export class JSON {
     }
 
     public static stringify(object:any):string {
-        return global.JSON.stringify(this.serialize(object));
+        return JSON.stringify(this.serialize(object));
     }
 }
