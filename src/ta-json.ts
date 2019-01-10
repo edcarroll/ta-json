19c910995513aa6db37fb0e844ced95aa337ac17
cavarzan <deividi.cavarzan@gmail.com>
2019-01-10 18:01:08 -0200

    fix lint

--- src/ta-json.ts
+++ src/ta-json.ts
@@ -1,21 +1,21 @@
-import { serialize } from './methods/serialize';
-import { deserialize } from './methods/deserialize';
-import { IParseOptions } from './types';
+import { serialize } from "./methods/serialize";
+import { deserialize } from "./methods/deserialize";
+import { IParseOptions } from "./types";
 
 export class TaJson {
-    public static deserialize<T>(object: any, type?: Function, options?: IParseOptions): T {
+    public static deserialize<T>(object:any, type?:Function, options?:IParseOptions):T {
         return deserialize(object, type, options);
     }
 
-    public static parse<T>(json: string, type?: Function, options?: IParseOptions): T {
+    public static parse<T>(json:string, type?:Function, options?:IParseOptions):T {
         return this.deserialize<T>(JSON.parse(json), type, options);
     }
 
-    public static serialize(value: any, type?: Function): any {
+    public static serialize(value:any, type?:Function):any {
         return serialize(value, type);
     }
 
-    public static stringify(object: any, type?: Function): string {
+    public static stringify(object:any, type?:Function):string {
         return JSON.stringify(this.serialize(object, type));
     }
 }
